ac741ce32b4161f85e0c1ddef920597229548b19
Daniel Lee <lihongyangnju@gmail.com>
2026-01-09 16:37:27 +0800

    Initial Commit

---
+++ 合同扫描王-快速读懂合同&识别风险/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  合同扫描王-快速读懂合同&识别风险
+//
+//  Created by Mercury on 2026/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
